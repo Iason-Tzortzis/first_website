b81d4ccad693a9afff122724e7d241af688bd37e
Iason-Tzortzis <iason.tzortzis@gmail.com>
2021-07-12 18:29:30 +0300

    updates

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
 app.engine('ejs', ejsMate)
+app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+
+app.post('/', (req, res) => {
+    console.log(req.body)
 })
 
 
